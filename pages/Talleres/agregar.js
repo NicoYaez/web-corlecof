86da7c85ed76b2af561bc01f79f1e3cd8fc32068
NicoYaez <nicoyaez171@gmail.com>
2024-06-25 08:04:36 +0000

    Update

--- pages/Talleres/agregar.js
+++ pages/Talleres/agregar.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import Link from 'next/link';
-import styles from '../styles/Agregar.module.css';
+import styles from '../../styles/Agregar.module.css';
 
 const AgregarAlumno = () => {
     const router = useRouter();
